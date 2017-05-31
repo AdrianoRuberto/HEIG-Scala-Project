d570b96ad93682f432b79e6da134dc7e9c88d12a
Adriano Ruberto <adriano.ruberto@heig-vd.ch>
2017-05-31 21:01:12 +0200

    Add the basic vector function

--- shared/src/main/scala/engine/geometry/Vector.scala
+++ shared/src/main/scala/engine/geometry/Vector.scala
@@ -1,10 +1,28 @@
 package engine.geometry
 
 case class Vector(x: Double, y: Double) {
-	@inline def distance(p: Vector): Double = distance(p.x, p.y)
-	@inline def distance(u: Double, v: Double): Double = Math.sqrt(squaredDistance(u, v))
-	@inline def squaredDistance(p: Vector): Double = squaredDistance(p.x, p.y)
-	@inline def squaredDistance(u: Double, v: Double): Double = g.squaredDistance(x, y, u, v)
+	def + (v: Vector): Vector = Vector(x + v.x, y + v.y)
+	def - (v: Vector): Vector = Vector(x - v.x, y - v.y)
 
-	def cross(v: Vector): Double = x * v.x - y * v.y
+	def unary_-(): Vector = Vector(-x, -y)
+
+	def * (v: Vector): Double = x * v.x + y * v.y
+
+	def * (k: Double): Vector = Vector(x * k, y * k)
+	def / (k: Double): Vector = Vector(x / k, y / k)
+
+	def cross(v: Vector): Double = x * v.y - y * v.x
+	def norm: Double = math.sqrt(x * x + y * y)
+	def project(v: Vector): Vector = v * (this * v)
+	def perpendicular: Vector = Vector(-y, x)
+
+	@inline def squaredDistance(v: Vector): Double = squaredDistance(v.x, v.y)
+	@inline def squaredDistance(u: Double, v: Double): Double = g.squaredDistance(x, y, u, v)
+	@inline def distance(v: Vector): Double = distance(v.x, v.y)
+	@inline def distance(u: Double, v: Double): Double = Math.sqrt(squaredDistance(u, v))
+
+	def rotate(rad: Double): Vector = Vector(x * math.cos(rad) - y * math.sin(rad), x * math.sin(rad) + y * math.cos(rad))
+
+	def isZero: Boolean = x == 0 && y == 0
+	def notZero: Boolean = x != 0 || y != 0
 }
