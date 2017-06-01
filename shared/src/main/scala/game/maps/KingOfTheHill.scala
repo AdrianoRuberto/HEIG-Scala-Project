b4db59c1b16293bedc3b002afae2f6745427de8c
Bastien Clément <g@ledric.me>
2017-06-01 12:57:20 +0200

    Saner map geometry construction

--- shared/src/main/scala/game/maps/KingOfTheHill.scala
+++ shared/src/main/scala/game/maps/KingOfTheHill.scala
@@ -5,31 +5,28 @@ import scala.language.implicitConversions
 
 private[maps] object KingOfTheHill {
 
-	private val size: Double = 20
-	private implicit val wallColor = ColoredShape.DefaultColor("#333")
-
-	val illios = Seq[ColoredShape](
+	val illios = Seq(
 		// Left side
-		Segment(-22, -13, -22, 25) scale size,
-		Segment(-22, 7, -7, 7) scale size,
-		Rectangle(-7, 5, 2, 2) scale size,
-		Rectangle(-18, -3, 6, 7) scale size,
-		Rectangle(-10, -3, 5, 4) scale size,
-		Rectangle(-13, -10, 7, 4) scale size,
-		Triangle(-7, 7, -10, 7, -7, 5) scale size,
+		Segment(-22, -13, -22, 25),
+		Segment(-22, 7, -7, 7),
+		Rectangle(-7, 5, 2, 2),
+		Rectangle(-18, -3, 6, 7),
+		Rectangle(-10, -3, 5, 4),
+		Rectangle(-13, -10, 7, 4),
+		Triangle(-7, 7, -10, 7, -7, 5),
 		// Center
-		Circle(0, 0, 3) scale size,
-		Segment(-22, -13, 22, -13) scale size,
-		Segment(-4, -11, 4, -11) scale size,
-		Segment(-4, -11, -4, -9) scale size,
-		Segment(-4, -7, -4, -5) scale size,
-		Segment(4, -11, 4, -9) scale size,
-		Segment(4, -7, 4, -5) scale size,
-		Segment(-4, -5, -1, -5) scale size,
-		Segment(4, -5, 1, -5) scale size,
-		Triangle(0, -12, -1, -11, 1, -11) scale size,
+		Circle(0, 0, 3),
+		Segment(-22, -13, 22, -13),
+		Segment(-4, -11, 4, -11),
+		Segment(-4, -11, -4, -9),
+		Segment(-4, -7, -4, -5),
+		Segment(4, -11, 4, -9),
+		Segment(4, -7, 4, -5),
+		Segment(-4, -5, -1, -5),
+		Segment(4, -5, 1, -5),
+		Triangle(0, -12, -1, -11, 1, -11),
 		// Right side
 
-		Segment(22, -13, 22, 7) scale size
-	)
+		Segment(22, -13, 22, 7)
+	).map(_ scale 20 colored "#333")
 }
