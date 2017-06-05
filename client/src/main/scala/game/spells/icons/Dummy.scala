8bf409016a00cdcc7dd7d8fdc576cc0e04df9850
Bastien Clément <g@ledric.me>
2017-06-05 23:49:21 +0200

    Fix action button text alignment

--- client/src/main/scala/game/spells/icons/Dummy.scala
+++ client/src/main/scala/game/spells/icons/Dummy.scala
@@ -4,9 +4,11 @@ import engine.CanvasCtx
 
 case class Dummy(label: String) extends SpellIcon {
 	protected def drawIcon(ctx: CanvasCtx): Unit = {
+		ctx.save()
 		ctx.textAlign = "center"
 		ctx.textBaseline = "middle"
 		ctx.font = "400 12px 'Roboto Mono'"
-		ctx.fillText(label, 30, 30)
+		ctx.fillText(label, 30, 30, 60)
+		ctx.restore()
 	}
 }
